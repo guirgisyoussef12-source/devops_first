8dbb8c0a7bde4d7f5989b04b4ba92eca1d6f30ad
guirgis <guirgisyoussef12@gmail.com>
2026-08-05 03:03:44 -0700

    first commit

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -72,12 +72,17 @@ WSGI_APPLICATION = 'todoApp.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/2.2/ref/settings/#database
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": 'localhost',
+        "PORT": '5432',
+        "NAME": 'actions',
+        "USER": 'postgres',
+        "PASSWORD": 'guirgis',
     }
 }
 
